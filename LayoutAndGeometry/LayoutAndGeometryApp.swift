d2e51eed910d2346e9d9e63d9387ee38d48bc8e6
Kenneth Rathbun <kenrathbun1@gmail.com>
2024-06-19 22:44:41 -0700

    Initial Commit

---
+++ LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Kenneth Oliver Rathbun on 6/19/24.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
